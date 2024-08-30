eeb78e8676590ccfd13329b336db881902ccd588
Rebecca Allford <rebeccaallford1@gmail.com>
2024-08-30 15:27:51 +0000

    setting up env.py and security

--- blogging_again/settings.py
+++ blogging_again/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-if os.path.isfile("env.py"): import env
+if os.path.isfile("env.py"): 
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rnng)*916#p+khe+7!qgxfzyu_egcmy4k#_slvw_nf1x9fu!ps'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net', 
 '.herokuapp.com']
@@ -95,6 +96,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+ "https://*.8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net",
+ "https://*.herokuapp.com"
+]
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
